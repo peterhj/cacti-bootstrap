e496e8c6aa7475259e387c918ef1b4b71da515d0
Peter Jin <>
2023-07-12 19:07:52 -0400

    Only need one make invocation to build and install futhark.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,4 @@ set -eu -o pipefail
 cacti_bootstrap_commit="$(git show --format='%H'|head -1)"
 echo "- cacti-bootstrap: bootstrapping from git submodules at commit ${cacti_bootstrap_commit}..."
 git submodule update --init --recursive
-make -C futhark
 make -C futhark install
