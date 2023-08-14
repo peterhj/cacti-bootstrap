d67bc7da96a8bbf5218bdb6847389cbc06426167
Peter Jin <>
2023-08-14 15:23:19 -0400

    Update submodules and bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 set -eu -o pipefail
 cacti_bootstrap_commit="$(git show --format='%H'|head -1)"
-echo "- cacti-bootstrap: bootstrapping from git submodules at commit ${cacti_bootstrap_commit}..."
+
+echo "- cacti-bootstrap: Bootstrapping from git submodules at commit ${cacti_bootstrap_commit}..."
+set -x
 git submodule update --init --recursive
+{ set +x;} 2>&-
+
+echo "- cacti-bootstrap: Configuring cacti-futhark (note: this calls 'cabal update')..."
+set -x
+make -C futhark configure
+{ set +x;} 2>&-
+
+echo "- cacti-bootstrap: Building and installing cacti-futhark to cabal bin path..."
+set -x
 make -C futhark install
+{ set +x;} 2>&-
+
+echo "- cacti-bootstrap: Bootstrap complete."
+echo "- cacti-bootstrap: Now, you may build cacti for development as follows:"
+echo "- cacti-bootstrap:"
+echo "- cacti-bootstrap:     cd cacti"
+echo "- cacti-bootstrap:     make"
+echo "- cacti-bootstrap:"
+echo "- cacti-bootstrap: Or, if you would like a release build:"
+echo "- cacti-bootstrap:"
+echo "- cacti-bootstrap:     cd cacti"
+echo "- cacti-bootstrap:     make release"
+echo "- cacti-bootstrap:"
